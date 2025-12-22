0a28927e43c946f73e4b41ea176f46b4213e1d7f
helenajr <helenajrobinson@gmail.com>
2025-12-22 10:59:34 +0000

    switched filepaths to environmental variable for consistency

--- predict.py
+++ predict.py
@@ -6,8 +6,12 @@ from sqlalchemy import create_engine, text
 
 # change this manually when you want to update the model
 model_version = "2025-10-17_kinkajou.joblib"
-base_path = r"S:\Finance & Performance\IM&T\BIReporting\Data science projects\Case Duration - Theatre case duration prediction\models"
-model_path = os.path.join(base_path, model_version)
+
+folder_path = os.environ.get('DS_FOLDER_PATH')
+if not folder_path:
+    raise ValueError("DS_FOLDER_PATH environment variable not set!")
+
+model_path = os.path.join(folder_path, "Case Duration - Theatre case duration prediction","models", model_version)
 
 # import data needing predictions (unbooked waitlist patients) and create a backup of the predictions table
 dsn = "coch_p2" 
@@ -17,7 +21,7 @@ new_data = pd.read_sql_query(sql_query, read_connection)
 
 backup_query = "select * from InformationSandpitDB.datascience.CaseDuration_predictions"
 backup_data = pd.read_sql_query(backup_query, read_connection)
-backup_path = r"S:\Finance & Performance\IM&T\BIReporting\Data science projects\Case Duration - Theatre case duration prediction\predictions_backup\CaseDuration_predictions.pkl"
+backup_path = os.path.join(folder_path, "Case Duration - Theatre case duration prediction","predictions_backup", "CaseDuration_predictions.pkl")
 backup_data.to_pickle(backup_path)
 
 read_connection.close()
